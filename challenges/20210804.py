8b5be596141c85dc60178513c6b33b55ce207b93
Pierre Gober <pierregober@gmail.com>
2021-08-04 18:59:26 +0000

    initial for project

---
+++ challenges/20210804.py
@@ -0,0 +1,6 @@
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+for animals in farms[0]:
+    print(animals)
---
+++ python-project/age-guesser.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import random
+
+print("You are now taking the quiz: ")
+print("\"Let me guess how old you are by telling me the programming languages you know\"")
+
+
+
+##Do you know how to code using FORTRAN or COBOL?
+## [0] Yes, [1]No, [2] Neiher 
+##Okay which one? [1] FORTRAN  [2]COBOL [3] I lied.
+
+##Do you know how to code using BASIC or PASCAL?
+## [0] Yes, [1]No, [2] Neiher 
+##Okay which one? [1] FORTRAN  [2]COBOL [3] I lied.
+
+##Do you know how to code using C?
+#Note: NOT C++ nor C#
+## [0] Yes, [1]No, [2] Neiher 
+##Okay which one? [1]   [2] [3] I lied.
+
+
+ans_none = "neither"
+
