b6bb6d2c77de057759356e97e8ed9da30903497e
CleverlyDone <pierregober@gmail.com>
2021-08-05 18:39:03 -0700

    update for project 2

--- pokeman.py
+++ pokeman.py
@@ -2,7 +2,7 @@
 import csv
 import random
 
-print("  █████▒██▓ ███▄    █ ▓█████▄    ▓██   ██▓ ▒█████   █    ██  ██▀███      ██▓███  ▒█████   ██ ▄█▀▓█████ ███▄ ▄███▓ ▒█████   ███▄    █")
+print("\n\n\n  █████▒██▓ ███▄    █ ▓█████▄    ▓██   ██▓ ▒█████   █    ██  ██▀███      ██▓███  ▒█████   ██ ▄█▀▓█████ ███▄ ▄███▓ ▒█████   ███▄    █")
 print("▓██   ▒▓██▒ ██ ▀█   █ ▒██▀ ██▌    ▒██  ██▒▒██▒  ██▒ ██  ▓██▒▓██ ▒ ██▒   ▓██░  ██▒██▒  ██▒ ██▄█▒ ▓█   ▀▓██▒▀█▀ ██▒▒██▒  ██▒ ██ ▀█   █ ")
 print("▒████ ░▒██▒▓██  ▀█ ██▒░██   █▌     ▒██ ██░▒██░  ██▒▓██  ▒██░▓██ ░▄█ ▒   ▓██░ ██▓▒██░  ██▒▓███▄░ ▒███  ▓██    ▓██░▒██░  ██▒▓██  ▀█ ██▒")
 print("░▓█▒  ░░██░▓██▒  ▐▌██▒░▓█▄   ▌     ░ ▐██▓░▒██   ██░▓▓█  ░██░▒██▀▀█▄     ▒██▄█▓▒ ▒██   ██░▓██ █▄ ▒▓█  ▄▒██    ▒██ ▒██   ██░▓██▒  ▐▌██▒")
@@ -21,13 +21,13 @@ print("|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|/__\|/__\|")
 
 def catch_em_all():
     selection = input(
-        "\n[Flying, Water, Electric, Grass, Rock, Poison, Fairy, Psychic, Dragon]\nWhat kind of Pokeman do you want to view?\n > ")
+        "\n[Flying, Water, Fire, Electric, Grass, Rock, Poison, Fairy, Psychic, Dragon]\nWhat kind of Pokeman do you want to view?\n > ")
 
     with open("pokedex.txt", "r") as csvfile:
         # to make our file unqiue will be used later
         i = 0
         collector = []
-        max = 720
+        max = 719
         # iterate through my data set\
         for row in csv.reader(csvfile):
 
@@ -37,13 +37,14 @@ def catch_em_all():
             # give me all the pokemon that are whatever the user inputed
             # python has a "in" operator for list that checks for a string within a list
             if i == max:
+                print(collector)
                 with open(filename, "w") as txt:
                     print(collector, file=txt)
-                    print(filename + ": Successfully written!!")
-                    # Let's run it again
-                    catch_em_all()
+                    # print(filename + ": Successfully written!!")
+                    # Let's run it again -- work in progress
+                    # catch_em_all()
             elif selection in row:
-                collector.append(row[0] + ": " + row[1])
+                collector.append("#" + row[0] + ": " + row[1])
 
 
 catch_em_all()
